54a031a290256f632d79a0321b0b4899cbdede84
Emmanuel Obiajulu Okoye <>
2021-04-06 19:49:45 +0000

    Initial commit

---
+++ BMI CALUCULATOR 1.py
@@ -0,0 +1,16 @@
+# asking for the user input and converting it to
+# converting the string to and integer
+# because an integer can be
+height = int(input ("Enter Your height in M: \n"))
+weight = int(input ("Enter Your weight in W: \n"))
+
+# calculating the actual number
+# we can said
+# bmiCalculator = int(weigth)/ float (height) **2
+bmiCalculator = int(height/(weight ** 2))
+
+# printing out the actual value
+  
+print (bmiCalculator)
+
+
